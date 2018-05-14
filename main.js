890f9451a78a8b2dac385f66a1517c1e95df281d
Matthias Kleine <matthias@klein0r.de>
2018-05-14 19:25:18 +0200

    Meta-Data / Device infos

--- main.js
+++ main.js
@@ -1,47 +1,12 @@
-/**
- *
- * lametric adapter
- *
- *
- *  file io-package.json comments:
- *
- *  {
- *      "common": {
- *          "name":         "lametric",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js lametric Adapter",  // Adapter title shown in User Interfaces
- *          "authors":  [                               // Array of authord
- *              "name <mail@lametric.com>"
- *          ]
- *          "desc":         "lametric adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
- *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
- *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
- *          "materialize":  true,                       // support of admin3
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
- *          "loglevel":     "info"                      // Adapters Log Level
- *      },
- *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
- *          "test1": true,
- *          "test2": 42,
- *          "mySelect": "auto"
- *      }
- *  }
- *
- */
-
 /* jshint -W097 */// jshint strict:false
 /*jslint node: true */
 'use strict';
 
-// you have to require the utils module and call adapter function
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require(__dirname + '/lib/utils');
+var request = require('request');
 
-// you have to call the adapter function and pass a options object
-// name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.lametric.0
 var adapter = new utils.Adapter('lametric');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
     try {
         adapter.log.info('cleaned everything up...');
@@ -51,18 +16,14 @@ adapter.on('unload', function (callback) {
     }
 });
 
-// is called if a subscribed object changes
 adapter.on('objectChange', function (id, obj) {
     // Warning, obj can be null if it was deleted
     adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
 });
 
-// is called if a subscribed state changes
 adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
     adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 
-    // you can use the ack flag to detect if it is status (true) or command (false)
     if (state && !state.ack) {
         adapter.log.info('ack is not set!');
     }
@@ -81,73 +42,87 @@ adapter.on('message', function (obj) {
     }
 });
 
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
     main();
 });
 
 function main() {
 
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-    adapter.log.info('config test1: '    + adapter.config.test1);
-    adapter.log.info('config test1: '    + adapter.config.test2);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+    adapter.log.info('ip: ' + adapter.config.lametricIp);
 
+    // Refresh State every Minute
+    refreshState();
+    setInterval(refreshState, 60000);
 
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple lametric for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
-
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
-
-    // in this lametric all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
+}
 
+function refreshState()
+{
+    adapter.log.debug('refreshing LaMetric state');
 
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
+    buildRequest(
+        'device',
+        function(content) {
+            adapter.setState('info.connection', true, true);
 
-    // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
+            adapter.setState('meta.name', {val: content.name, ack: true});
+            adapter.setState('meta.serial', {val: content.serial_number, ack: true});
+            adapter.setState('meta.version', {val: content.os_version, ack: true});
+            adapter.setState('meta.model', {val: content.model, ack: true});
+            adapter.setState('meta.mode', {val: content.mode, ack: true});
 
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
+            adapter.setState('meta.audio.volume', {val: content.audio.volume, ack: true});
 
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+            adapter.setState('meta.bluetooth.available', {val: content.bluetooth.available, ack: true});
+            adapter.setState('meta.bluetooth.name', {val: content.bluetooth.name, ack: true});
+            adapter.setState('meta.bluetooth.active', {val: content.bluetooth.active, ack: true});
+            adapter.setState('meta.bluetooth.discoverable', {val: content.bluetooth.discoverable, ack: true});
+            adapter.setState('meta.bluetooth.pairable', {val: content.bluetooth.pairable, ack: true});
+            adapter.setState('meta.bluetooth.address', {val: content.bluetooth.address, ack: true});
 
+            adapter.setState('meta.display.brightness', {val: content.display.brightness, ack: true});
+            adapter.setState('meta.display.brightness_mode', {val: content.display.brightness_mode, ack: true});
+            adapter.setState('meta.display.width', {val: content.display.width, ack: true});
+            adapter.setState('meta.display.height', {val: content.display.height, ack: true});
+            adapter.setState('meta.display.type', {val: content.display.type, ack: true});
 
+            adapter.setState('meta.wifi.active', {val: content.wifi.active, ack: true});
+            adapter.setState('meta.wifi.address', {val: content.wifi.address, ack: true});
+            adapter.setState('meta.wifi.available', {val: content.wifi.available, ack: true});
+            adapter.setState('meta.wifi.encryption', {val: content.wifi.encryption, ack: true});
+            adapter.setState('meta.wifi.ssid', {val: content.wifi.essid, ack: true});
+            adapter.setState('meta.wifi.ip', {val: content.wifi.ip, ack: true});
+            adapter.setState('meta.wifi.mode', {val: content.wifi.mode, ack: true});
+            adapter.setState('meta.wifi.netmask', {val: content.wifi.netmask, ack: true});
+            adapter.setState('meta.wifi.strength', {val: content.wifi.strength, ack: true});
+        }
+    );
+}
 
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
+function buildRequest(service, callback)
+{
+    var url = 'http://' + adapter.config.lametricIp + ':8080/api/v2/' + service;
 
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
-
-
-
+    request(
+        {
+            url: url,
+            method: "GET",
+            json: true,
+            auth: {
+                user: 'dev',
+                pass: adapter.config.lametricToken,
+                sendImmediately: true
+            }
+        },
+        function(error, response, content) {
+            if (!error && response.statusCode == 200) {
+               callback(content);
+            } else if (error) {
+                adapter.log.error(error);
+            } else {
+                adapter.log.error('Status Code: ' + response.statusCode + ' / Content: ' + content);
+            }
+        }
+    );
 }
