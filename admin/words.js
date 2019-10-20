4f2f8a691706018d19888818bf8fc61d0624b4c9
matthias@klein0r.de <Matthias Kleine>
2019-10-20 16:26:36 +0200

    Updated io-package and admin

--- admin/words.js
+++ admin/words.js
@@ -2,8 +2,20 @@
 'use strict';
 
 systemDictionary = {
-    "IP": {                                          "en": "IP",                                              "de": "IP"},
-    "IP address of your LaMetric Time": {            "en": "IP address of your LaMetric Time",                "de": "IP-Adresse der LaMetric Time"},
-    "Developer Token": {                             "en": "Developer Token",                                 "de": "Entwickler Token"},
-    "Your personal developer token - you can get it <a href=\"https://developer.lametric.com/\">here</a>": {"en": "Your personal developer token - you can get it <a href=\"https://developer.lametric.com/\">here</a>", "de": "Persönliches Entwickler-Token, welches Du <a href=\"https://developer.lametric.com/\">hier</a> bekommst"},
+    "IP": {
+        "en": "IP",
+         "de": "IP"
+    },
+    "IP address of your LaMetric Time": {
+        "en": "IP address of your LaMetric Time",
+        "de": "IP-Adresse der LaMetric Time"
+    },
+    "Developer Token": {
+        "en": "Developer Token",
+        "de": "Entwickler Token"
+    },
+    "Your personal developer token - you can get it <a href=\"https://developer.lametric.com/\">here</a>": {
+        "en": "Your personal developer token - you can get it <a href=\"https://developer.lametric.com/\">here</a>",
+        "de": "Persönliches Entwickler-Token, welches Du <a href=\"https://developer.lametric.com/\">hier</a> bekommst"
+    }
 };
