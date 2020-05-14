357cc09f2d10718f11dd1cb657cb430b2f1e60c5
Matthias Kleine <info@haus-automatisierung.com>
2020-05-14 07:40:43 +0200

    Updated translations

--- admin/words.js
+++ admin/words.js
@@ -3,19 +3,51 @@
 
 systemDictionary = {
     "IP": {
-        "en": "IP",
-         "de": "IP"
+        "en": "IP-Address",
+        "de": "IP Adresse",
+        "ru": "Айпи адрес",
+        "pt": "Endereço de IP",
+        "nl": "IP adres",
+        "fr": "Adresse IP",
+        "it": "Indirizzo IP",
+        "es": "Dirección IP",
+        "pl": "Adres IP",
+        "zh-cn": "IP地址"
     },
     "IP address of your LaMetric Time": {
         "en": "IP address of your LaMetric Time",
-        "de": "IP-Adresse der LaMetric Time"
+        "de": "IP-Adresse Ihrer LaMetric Time",
+        "ru": "IP-адрес вашего LaMetric Time",
+        "pt": "Endereço IP do seu LaMetric Time",
+        "nl": "IP-adres van uw LaMetric Time",
+        "fr": "Adresse IP de votre LaMetric Time",
+        "it": "Indirizzo IP di LaMetric Time",
+        "es": "Dirección IP de su hora de LaMetric",
+        "pl": "Adres IP twojego czasu LaMetric",
+        "zh-cn": "您的LaMetric Time的IP地址"
     },
     "Developer Token": {
         "en": "Developer Token",
-        "de": "Entwickler Token"
+        "de": "Entwickler-Token",
+        "ru": "Жетон разработчика",
+        "pt": "Token de desenvolvedor",
+        "nl": "Ontwikkelaarstoken",
+        "fr": "Jeton de développeur",
+        "it": "Token sviluppatore",
+        "es": "Token de desarrollador",
+        "pl": "Token programisty",
+        "zh-cn": "开发者令牌"
     },
     "Your personal developer token - you can get it <a href=\"https://developer.lametric.com/\">here</a>": {
         "en": "Your personal developer token - you can get it <a href=\"https://developer.lametric.com/\">here</a>",
-        "de": "Persönliches Entwickler-Token, welches Du <a href=\"https://developer.lametric.com/\">hier</a> bekommst"
+        "de": "Ihr persönliches Entwickler-Token - Sie können es <a href=\"https://developer.lametric.com/\">hier erhalten</a>",
+        "ru": "Ваш личный токен разработчика - вы можете получить его <a href=\"https://developer.lametric.com/\">здесь</a>",
+        "pt": "Seu token de desenvolvedor pessoal - você pode obtê-lo <a href=\"https://developer.lametric.com/\">aqui</a>",
+        "nl": "Je persoonlijke ontwikkelaarstoken - je kunt het <a href=\"https://developer.lametric.com/\">hier downloaden</a>",
+        "fr": "Votre jeton de développeur personnel - vous pouvez l'obtenir <a href=\"https://developer.lametric.com/\">ici</a>",
+        "it": "Il tuo token sviluppatore personale: puoi ottenerlo <a href=\"https://developer.lametric.com/\">qui</a>",
+        "es": "Tu token de desarrollador personal: puedes obtenerlo <a href=\"https://developer.lametric.com/\">aquí</a>",
+        "pl": "Twój osobisty token programisty - możesz go zdobyć <a href=\"https://developer.lametric.com/\">tutaj</a>",
+        "zh-cn": "您的个人开发者令牌-您可以<a href=\"https://developer.lametric.com/\">在此处</a>获取"
     }
 };
